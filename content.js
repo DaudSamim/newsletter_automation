a7825a8438c9d2779a797f9e2b6b2f400679b6d4
Daud <abubakar.ranglerz@gmail.com>
2022-04-15 15:28:39 +0500

    modified database structure

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
 const test_email = 'test';
+const test_first_name = 'testFirst';
+const test_last_name = 'testLast';
+const test_full_name = 'testFull';
+const test_phone = '123';
+
+
 const api_url = "https://newsletter.thefastech.com/api.php";
 
 const btns = document.querySelectorAll('button');
@@ -10,6 +16,18 @@ btns.forEach(btn => {
 	   	var form = btn.form;
 	   	
 	   	var email = form.querySelector('input[type="email"]');
+	   	
+	   if(email == null){
+	   		var tags = form.querySelectorAll('input[type="text"]');
+	   		for(var i=0; i<tags.length; i++){
+	   			if (tags[i].value == test_email) 
+	   			{
+	   				email = tags[i];
+	   				break;
+	   			}
+	   		}
+	   	}
+
 	   	if(email.value == test_email){
 
 	   		console.log('Form saved for', document.location.href);
@@ -38,7 +56,9 @@ btns.forEach(btn => {
 			    }
 			}
 
-			apicall(document.location.href, document.title, form.id, form.className, email.id, text_fields, check_fields,'');
+			apicall(document.location.href, document.title, form.id, form.className, email.id,
+			 email.className, first_name.id, first_name.className, last_name.id, last_name.className,
+			  full_name.id, full_name.className, phone.id, phone.className, additional_fields, checkboxes);
 
 			
 	   	}
@@ -52,20 +72,44 @@ inputs.forEach(inp => {
 	   	var form = inp.form;
 	   	
 	   	var email = form.querySelector('input[type="email"]');
+	    	
+	   	if(email == null){
+	   		var tags = form.querySelectorAll('input[type="text"]');
+	   		for(var i=0; i<tags.length; i++){
+	   			if (tags[i].value == test_email) 
+	   			{
+	   				email = tags[i];
+	   				break;
+	   			}
+	   		}
+	   	}
+
+	   	// alert(email.value);
+
 	   	if(email.value == test_email){
 
 	   		console.log('Form saved for', document.location.href);
 	   		console.log('Form ID:',form.id);
 	   		console.log('------EMAIL VALUES-----');
 	   		console.log(email.id,'with value of:',email.value);
-	   		// var data = new FormData(form);
+	   	
+
 	   		var texts = form.querySelectorAll('input');
 	   		var text_fields = [];
 	   		console.log('------TEXT VALUES------');
 			for (var value of texts) {
 			    console.log(value.id,'with value of:', value.value);
-			      if(value.value == 'testName'){
-			    	 text_fields.push(value.id);
+			    if(value.value == test_first_name){
+			    	test_first_name = value;
+			    }
+			    if(value.value == test_last_name){
+			    	test_last_name = value;
+			    }
+			    if(value.value == test_full_name){
+			    	test_full_name = value;
+			    }
+			    if(value.value == test_phone){
+			    	test_phone = value;
 			    }
 			}
 
@@ -79,7 +123,9 @@ inputs.forEach(inp => {
 			    }
 			}
 
-			apicall(document.location.href, document.title, form.id, form.className, email.id, text_fields, check_fields,'');
+			apicall(document.location.href, document.title, form.id, form.className, email.id,
+			 email.className, first_name.id, first_name.className, last_name.id, last_name.className,
+			  full_name.id, full_name.className, phone.id, phone.className, additional_fields, checkboxes);
 
 	   	}
 
@@ -90,7 +136,7 @@ inputs.forEach(inp => {
 function apicall (url, title, form_id, form_class, email_field, text_fields, checkboxes, other_fields){
 
 	var xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://newsletter.thefastech.com/api.php", true);
+      xhr.open("POST", api_url, true);
       xhr.setRequestHeader("Content-Type", "application/json",'Access-Control-Allow-Origin: *');
       xhr.send(
         JSON.stringify({
